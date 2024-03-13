6b1d004e7c79bf2cead610b627944e6eb43f6aaa
lai2-24 <lai2-24@itismeucci.com>
2024-03-13 09:40:10 +0000

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './test/test.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,10 +8,10 @@ import { TableComponent } from './table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent, TableComponent, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, TableComponent, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular3';
+  title = 'angular4';
 }
